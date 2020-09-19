69165628a8c5327f5310ffd0fb586df890fd2505
Marwan Abdelmoaty <me@mrw4n.com>
2020-09-19 16:54:59 +0200

    creating routes

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Home Page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
